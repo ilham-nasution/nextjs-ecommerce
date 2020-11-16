481bfbd1d8dd67265b61fbde4a96803e22102928
Ilham Rahman <ilhaamrahman@gmail.com>
2020-11-16 08:46:52 +0700

    add delete button on cart

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -70,65 +70,94 @@ const Total = styled.div`
   }
 `;
 
+const DeleteBtn = styled.button`
+  background-color: transparent;
+  outline: none;
+  border: none;
+  border-bottom: 1px solid black;
+  padding: 0;
+`;
+
 const cart = () => {
-  const { cartItems } = useContext(CartContext);
+  const { cartItems, setCartItems } = useContext(CartContext);
 
   const subtotal = cartItems.reduce((total, item) => {
     return total + parseInt(item.price);
   }, 0);
+
+  const handleDelete = (id) => {
+    const updatedItems = cartItems.filter((item) => {
+      return item.id !== id;
+    });
+    setCartItems(updatedItems);
+  };
 
   return (
     <div>
       <Navbar />
       <Container>
         <h3>Your bag</h3>
-        <Row>
-          <Link href="/store/new">
-            <a>⬅ Keep shopping</a>
-          </Link>
-          <CartTable>
-            <TableHead>
-              <p>Product</p>
-              <p>Color</p>
-              <p>Size</p>
-              <p>Price</p>
-            </TableHead>
-            {cartItems.map((item) => (
-              <TableItem key={item.id}>
-                <div
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    width: "250px",
-                    fontSize: "smaller",
-                  }}
-                >
-                  <img
-                    src={`http://localhost:1337${item.image[0].formats.thumbnail.url}`}
-                    alt={item.name}
-                    width="80px"
-                    height="80px"
-                  />
-                  <p>{item.name}</p>
-                </div>
-                <p>{item.color}</p>
-                <p>{item.size}</p>
-                <p>Rp {item.price}</p>
-              </TableItem>
-            ))}
-          </CartTable>
-        </Row>
-        <Total>
-          <p>
-            Shipping costs will be calculated once you have provided your
-            address.
-          </p>
-          <div>
-            <h4>Total</h4>
-            <h4>Rp {subtotal}</h4>
-          </div>
-          <Button>Checkout</Button>
-        </Total>
+        {cartItems.length > 0 ? (
+          <>
+            <Row>
+              <Link href="/store/new">
+                <a>⬅ Keep shopping</a>
+              </Link>
+              <CartTable>
+                <TableHead>
+                  <p>Product</p>
+                  <p>Color</p>
+                  <p>Size</p>
+                  <p>Price</p>
+                </TableHead>
+                {cartItems.map((item) => (
+                  <TableItem key={item.id}>
+                    <div
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        width: "250px",
+                        fontSize: "smaller",
+                      }}
+                    >
+                      <img
+                        src={`http://localhost:1337${item.image[0].formats.thumbnail.url}`}
+                        alt={item.name}
+                        width="80px"
+                        height="80px"
+                      />
+                      <p>{item.name}</p>
+                    </div>
+                    <p>{item.color}</p>
+                    <p>{item.size}</p>
+                    <p>Rp {item.price}</p>
+                    <DeleteBtn onClick={() => handleDelete(item.id)}>
+                      Delete
+                    </DeleteBtn>
+                  </TableItem>
+                ))}
+              </CartTable>
+            </Row>
+            <Total>
+              <p>
+                Shipping costs will be calculated once you have provided your
+                address.
+              </p>
+              <div>
+                <h4>Total</h4>
+                <h4>Rp {subtotal}</h4>
+              </div>
+              <Button>Checkout</Button>
+            </Total>
+          </>
+        ) : (
+          <>
+            <Link href="/store/new">
+              <a>⬅ Keep shopping</a>
+            </Link>
+            <h1>Your bag is currently empty</h1>
+          </>
+        )}
       </Container>
     </div>
   );
